ac64413e19b1c2e4471ae1b7e68f79bd23c3936d
Ralica Popova <ralicaterzieva90@gmail.com>
2025-08-10 18:40:42 +0300

    Inital commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI, HTTPException, Query, Depends
+
+app = FastAPI(title="Item Management API")
+
+@app.post("/items", response_model=schemas.Item, status_code=201)
+async def create_item(item: schemas.ItemCreate):
+    return crud.create_item(item)
